45cf3aba7f9a1ce8a2bd744ba5e51c5769e3e6a1
AlinaChikova <chikova.al@yandex.ru>
2019-08-20 15:43:34 +0300

    +card

--- src/app.js
+++ src/app.js
@@ -4,3 +4,4 @@ import './project/menu/menu.scss';
 import './project/header/header.scss';
 import './project/basket/basket.scss';
 import './project/frontpage/card.scss';
+import './project/frontpage/number.scss';
